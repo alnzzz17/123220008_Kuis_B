bb781d43cb4825033f1eae7a53f46e902a704f7a
Erllyta Rachma <erllyta107@gmail.com>
2025-03-11 09:39:38 +0700

    Fix: Title

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Latihan Kuis',
+      title: 'Kuis Praktikum TPM',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: LoginPage()
